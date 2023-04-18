d8d821cffccdfc642e5aa016fc434b5255dc8c06
MazzaAlessandro <10670112@polimi.it>
2023-04-18 14:00:21 +0200

    Player and movement got updated for the isometric view

--- TheGiantsCurse/Assets/Scripts/PlayerController.cs
+++ TheGiantsCurse/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float movementSpeed = 5f;
 
-    public Rigidbody2D rb;
-
-    Vector2 movement;
-
-    // This handles the input reception
+    private Vector3 movementInput;
+    
+    // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        movementInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
     }
 
-    // This handles the movement and execution of commands
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(transform.position + movementInput * movementSpeed * Time.deltaTime);
     }
 }
