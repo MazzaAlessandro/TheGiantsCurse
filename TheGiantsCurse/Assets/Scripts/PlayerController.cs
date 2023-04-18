505488dc5c3b4c44058d5de6a3ac4bfc2b18121b
MazzaAlessandro <10670112@polimi.it>
2023-04-18 15:28:29 +0200

    Added Helpers class, fixed isometric movement

--- TheGiantsCurse/Assets/Scripts/PlayerController.cs
+++ TheGiantsCurse/Assets/Scripts/PlayerController.cs
@@ -6,17 +6,29 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float turnSpeed = 720;
 
     private Vector3 movementInput;
+    
     
     // Update is called once per frame
     void Update()
     {
         movementInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        RotateLook();
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(transform.position + movementInput * movementSpeed * Time.deltaTime);
+        rb.MovePosition(transform.position + movementInput.ToIso() * movementSpeed * Time.fixedDeltaTime);
+    }
+
+    void RotateLook()
+    {
+        if (movementInput != Vector3.zero)
+        {
+            var rot = Quaternion.LookRotation(movementInput.ToIso(), Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnSpeed * Time.deltaTime);
+        }
     }
 }
