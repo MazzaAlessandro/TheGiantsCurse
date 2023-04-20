d312788956aa5fe4356bf50de97724a514b7bed8
MazzaAlessandro <10670112@polimi.it>
2023-04-20 18:04:08 +0200

    Camera follow, setup for aiming system

--- TheGiantsCurse/Assets/Scripts/Helper.cs
+++ TheGiantsCurse/Assets/Scripts/Helper.cs
@@ -7,4 +7,8 @@ public static class Helper
     private static Matrix4x4 isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
 
     public static Vector3 ToIso(this Vector3 input) => isoMatrix.MultiplyPoint3x4(input); 
+
+    public static Vector3 VerticalZero(this Vector3 mousePosition) => new Vector3(mousePosition.x, 0, mousePosition.z);
+
+    public static Vector3 IsoMousePosition(this Vector3 mousePosition) => new Vector3(0, 0, 0);
 }
--- TheGiantsCurse/Assets/Scripts/PlayerController.cs
+++ TheGiantsCurse/Assets/Scripts/PlayerController.cs
@@ -4,28 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody rb;
+    
     [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float aimingSpeed = 2.5f;
     [SerializeField] private float turnSpeed = 720;
 
+    private float speed;
+    private Rigidbody rb;
     private Vector3 movementInput;
-    
-    
+    private Vector3 AimDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        movementInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        RotateLook();
+        
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(transform.position + movementInput.ToIso() * movementSpeed * Time.fixedDeltaTime);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        movementInput = new Vector3(horizontal, 0, vertical);
+
+        if (Input.GetMouseButton(1))
+            speed = aimingSpeed;
+        else speed = movementSpeed;
+        rb.MovePosition(transform.position + movementInput.ToIso() * speed * Time.fixedDeltaTime);
+        RotateLook();
     }
 
     void RotateLook()
     {
-        if (movementInput != Vector3.zero)
+        if (Input.GetMouseButton(1))
+        {
+            //turn towards mouse direction
+
+        }
+        else if (movementInput != Vector3.zero)
         {
             var rot = Quaternion.LookRotation(movementInput.ToIso(), Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnSpeed * Time.deltaTime);
